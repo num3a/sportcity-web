c7571a76718703cf22f550776849c9955cdd9ad1
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2015-07-11 16:56:20 +0200

    add cors on /api routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var express = require('express'),
+    cors = require('cors'),
     router = express.Router();
 
-    router.get('/healthcheck', function(req,res){
+    router.get('/healthcheck',cors(), function(req,res){
         res.send({status: 'OK'});
 
     });
+
+router.get('/', function(req,res){
+    res.send({status: 'OK'});
+
+});
+
 
 module.exports = router;
