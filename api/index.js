5a468eeb2784a518ab0f8d6d896b59bbbe3062c7
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2015-07-11 17:30:22 +0200

    add log entries

--- api/index.js
+++ api/index.js
@@ -2,10 +2,18 @@
 
 var express = require('express'),
     cors = require('cors'),
-    router = express.Router();
+    router = express.Router(),
+    Logger = require('le_node');
+
+var logger = new Logger({
+    token: '0bded162-f6b7-47fc-aad4-372f9cb522b3'
+}),
 
     router.get('/healthcheck',cors(), function(req,res){
-        res.send({status: 'OK'});
+
+    var healthcheck = {status: 'OK'};
+    logger.info('Sportcity api is up', healthcheck);
+        res.send(healthcheck);
 
     });
 
